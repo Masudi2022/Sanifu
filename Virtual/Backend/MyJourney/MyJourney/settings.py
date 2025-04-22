ab7b489a040a02de10ba95f62a1c536b5073add0
Masudi2022 <salummasud440@gmail.com>
2025-04-22 17:53:29 -0400

    tw3 commit

--- Virtual/Backend/MyJourney/MyJourney/settings.py
+++ Virtual/Backend/MyJourney/MyJourney/settings.py
@@ -14,6 +14,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
 ALLOWED_HOSTS = os.environ.get('DJANGO_ALLOWED_HOSTS', '').split(',')
 
+
 if DEBUG:
     INTERNAL_IPS = ['127.0.0.1', 'localhost', '0.0.0.8000']
 
