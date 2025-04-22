392f73c07ced785d10effeba8772bed72fcf1bc9
Masudi2022 <salummasud440@gmail.com>
2025-04-22 14:01:31 -0400

    fouth commit

--- Virtual/Backend/MyJourney/MyJourney/urls.py
+++ Virtual/Backend/MyJourney/MyJourney/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
 
 urlpatterns = [
     
@@ -29,6 +30,8 @@ urlpatterns = [
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('', lambda request: HttpResponse("✅ Backend is working!")),
+    
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
