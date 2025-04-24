6e76840b8e392d7215d6c75b1f4e64586644fb34
Masudi2022 <salummasud440@gmail.com>
2025-04-24 18:38:56 -0400

    fb1 commit

--- Virtual/Frontend/journey/src/Virtual/Contact.jsx/Contact.jsx
+++ Virtual/Frontend/journey/src/Virtual/Contact.jsx/Contact.jsx
@@ -25,7 +25,7 @@ import { SiGmail } from 'react-icons/si';
 import axios from 'axios';
 import './Contact.css';
 
-const API_URL = `${import.meta.env.VITE_API_BASE_URL}/api/feedaback/`;
+const API_URL = `${import.meta.env.VITE_API_BASE_URL}/api/feedback/`;
 
 const Contact = () => {
   const [formData, setFormData] = useState({
