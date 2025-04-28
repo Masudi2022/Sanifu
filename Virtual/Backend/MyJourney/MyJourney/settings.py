a4ea6deecd1a01802d566e9c603087cc478a1d08
Masudi2022 <salummasud440@gmail.com>
2025-04-28 06:57:29 -0400

    secure2 commit

--- Virtual/Backend/MyJourney/MyJourney/settings.py
+++ Virtual/Backend/MyJourney/MyJourney/settings.py
@@ -128,7 +128,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Email settings
 import os
 
-EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND')
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
 EMAIL_HOST = os.environ.get('EMAIL_HOST')
 EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
 EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
