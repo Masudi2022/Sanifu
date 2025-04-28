6a93c86fc391747c748a548ebe129f4e4c3ae050
Masudi2022 <salummasud440@gmail.com>
2025-04-28 06:18:13 -0400

    secure commit

--- Virtual/Backend/MyJourney/MyJourney/settings.py
+++ Virtual/Backend/MyJourney/MyJourney/settings.py
@@ -126,13 +126,18 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # settings.py
 
 # Email settings
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'salummasud54@gmail.com'
-EMAIL_HOST_PASSWORD = 'zyvy wpfi bnhm vhmb'  # Your app password
-DEFAULT_FROM_EMAIL = 'salummasud54@gmail.com'
+import os
+
+EMAIL_BACKEND = os.environ.get('EMAIL_BACKEND')
+EMAIL_HOST = os.environ.get('EMAIL_HOST')
+EMAIL_PORT = int(os.environ.get('EMAIL_PORT', 587))
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS', 'True') == 'True'
+EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
+DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
+
+ADMIN_EMAIL = os.environ.get('ADMIN_EMAIL')
+
 
 
 
